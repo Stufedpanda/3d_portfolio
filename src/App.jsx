d07b7d188ffe5c5bc7768be2585fddae442a049a
Forbes Li <stufedpand@gmail.com>
2025-11-20 18:38:43 -0500

    Switched to HashRouter for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, HashRouter as Router, Routes, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { Home, About, Projects, Contact } from './pages'
 
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
-        <Router>
-            <Navbar />
-            <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/projects' element={<Projects/>} />
-            <Route path='/contact' element={<Contact/>} />
-            </Routes>
-        </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+          
+          {/* Fallback for any unknown route */}
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </Router>
     </main>
   )
 }
